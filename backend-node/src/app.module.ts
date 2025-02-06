4c4ef229542a5f69fbc9471ed8ea14d3a5ffcdb2
Victor Olivares <victor.olivares@apuestatotal.com>
2025-02-06 14:06:41 -0500

    feat: Agregar módulos y entidades para gestión de usuarios, roles, permisos y módulos en el backend

--- backend-node/src/app.module.ts
+++ backend-node/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
+import { PermissionModule } from './permission/permission.module';
+import { ModuleModule } from './module/module.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    RoleModule,
+    PermissionModule,
+    ModuleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
