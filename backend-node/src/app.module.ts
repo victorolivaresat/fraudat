d99736a129d4c61de0c4353f35460a7c8b827363
Victor Olivares <victor.olivares@apuestatotal.com>
2025-02-06 14:14:51 -0500

    refactor: Eliminar módulos y entidades de gestión de usuarios, roles, permisos y módulos en el backend

--- backend-node/src/app.module.ts
+++ backend-node/src/app.module.ts
@@ -1,25 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { RoleModule } from './role/role.module';
-import { PermissionModule } from './permission/permission.module';
-import { ModuleModule } from './module/module.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database.sqlite',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    UserModule,
-    RoleModule,
-    PermissionModule,
-    ModuleModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
